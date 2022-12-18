6b40d28caf35b5657a1fd729ff45bde0d416f518
bartstc <bartst010395@gmail.com>
2022-12-18 11:35:10 +0100

    Config - configure eslint, prettier, husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "@next/font/google";
+
+import styles from "../styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -26,7 +28,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +121,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
