290d992ca2d7d8f0984b47cb95d4203076805b1d
bartstc <bartst010395@gmail.com>
2022-12-31 13:24:38 +0100

    Remove unused styles and functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,6 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 
-import "../styles/globals.css";
-
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
